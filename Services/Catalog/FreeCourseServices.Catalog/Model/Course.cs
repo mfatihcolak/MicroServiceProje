6036a9bc55a2aee90e1b680ac80a57af782952a5
Muhammed Fatih Çolak <mfatihcolak1@gmail.com>
2022-09-05 15:44:40 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourseServices.Catalog/Model/Course.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace FreeCourseServices.Catalog.Model
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+        public Feature Feature { get; set; }
+        public string CategoryId { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
